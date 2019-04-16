98609a48a61e1b10a380b7fc75b897eddf91c34d
domw30 <domw30@gmail.com>
2019-04-16 16:54:26 +0100

    add feature test to testing_infrastructure

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/Battle' do
-    'Hello World'
+  get '/' do
+    erb :index
   end
 
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
-ENV ['RACK_ENV'] = 'test'
+ENV['RACK_ENV'] = "test"
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
-require 'capybara/spec'
+require 'capybara/rspec'
 require 'rspec'
 
 Capybara.app = Battle
