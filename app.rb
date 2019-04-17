7bea141f33a19ad4f6231899c1f33aac34d039c9
domw30 <domw30@gmail.com>
2019-04-17 14:54:12 +0100

    store player names in the session

--- app.rb
+++ app.rb
@@ -2,13 +2,17 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable: sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
     erb :play
   end
+
+  
 end
