66cfc2a2ec237e67e813b528f1661352cdf106ad
domw30 <domw30@gmail.com>
2019-04-17 16:40:13 +0100

    add test for attack, add attack.erb file, add form to play.erb with action /attack

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,4 @@
-
-
-
-#Capybara feature test
-
+# capybara tests
 
 feature 'Enter names' do
   scenario 'submitting names' do
@@ -15,5 +11,13 @@ feature 'View Hit Points' do
   scenario 'display player 2 HP' do
     sign_in_and_play
     expect(page).to have_content 'Ben: 200 HP'
+  end
+end
+
+feature 'Attacking' do
+  scenario 'Attack player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Ben attacked Dom'
   end
 end
